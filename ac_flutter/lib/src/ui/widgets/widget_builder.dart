103a633a5635c2f1f6e4073fb2b77b004c37c5c4
Martin Edlman <ac@an.y-co.de>
2024-01-14 15:06:51 +0100

    Add TypedWidgetBuilder method

--- ac_flutter/lib/src/ui/widgets/widget_builder.dart
+++ ac_flutter/lib/src/ui/widgets/widget_builder.dart
@@ -3,3 +3,6 @@ import 'package:flutter/widgets.dart';
 
 /// Widget builder with optional [child] (might be some static widget prepared before calling [builder])
 typedef AcWidgetBuilder = Widget Function(BuildContext context, Widget? child);
+
+/// Widget builder with an optional [data] of type T and a [child] (might be some static widget prepared before calling [builder])
+typedef AcTypedWidgetBuilder<T> = Widget Function(BuildContext context, T? data, Widget? child);
