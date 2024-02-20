e5693cbdedaf434315be3ac49f12752f7fec8d6d
Martin Edlman <ac@an.y-co.de>
2024-02-20 16:06:25 +0100

    * Update pkg dep on ac_dart * Update versions to 0.1.4. * Separate pkg overrides to `pubspec_overrides.yaml`

--- lib/src/debug/debug_service.dart
+++ lib/src/debug/debug_service.dart
@@ -1,35 +1,8 @@
-/*
- * MIT License
- *
- * Copyright (c) 2023 FORM08.COM s.r.o.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * Project: Form08
- * Module: ac_flutter
- * File: debug_service.dart
- *
- */
-
+import 'dart:async';
 import 'dart:io';
 
 import 'package:ac_dart/ac_dart.dart';
+import 'package:ac_flutter/ac_flutter.dart';
 import 'package:flutter/widgets.dart';
 import 'package:logger/logger.dart';
 import 'package:media_storage/media_storage.dart';
@@ -38,88 +11,206 @@ import 'package:package_info_plus/package_info_plus.dart';
 class DebugService {
   static const defaultPath = '{pkg}/files/logs';
 
-  /// Logger created in the default constructor used for logging generic app events
-  late DebugLogger appLogger;
+  static const _apiLoggerName = 'api';
+  static const _performanceLoggerName = 'performance';
+  static const _appLoggerName = 'app';
+  static const _errorLoggerName = 'error';
 
-  /// Logger created in the default constructor used for logging API events
-  late DebugLogger apiLogger;
+  /// Logger created in the default constructor used for logging generic app events.
+  /// If loggers are initialized from constructor, this logger may not be initialized yet when used,
+  /// so it's better to await [appLoggerAsync] to complete.
+  DebugLogger get appLogger => _loggers.containsKey(_appLoggerName)
+      ? _loggers[_appLoggerName]!
+      : throw StateError('App Logger not initialized yet, either await for `appLoggerAsync` or await for `init()` to complete');
 
-  /// Logger created in the default constructor used for logging performance
-  late DebugLogger performanceLogger;
+  /// Async logger created in the default constructor used for logging generic app events.
+  /// This logger is initialized immediately in the constructor, you need to await for it to complete
+  /// when needed. If you need logger without waiting, use [appLogger] instead. It's your responsibility
+  /// to make sure the logger is initialized, eg. by awaiting for `init()` to complete.
+  Future<DebugLogger> get appLoggerAsync => _loggersCompleters.containsKey(_appLoggerName)
+      ? _loggersCompleters[_appLoggerName]!.future
+      : throw StateError('App Logger does not exist');
 
-  /// Logger created in the default constructor used for logging errors
-  late DebugLogger errorLogger;
+  /// Logger created in the default constructor used for logging generic API events.
+  /// If loggers are initialized from constructor, this logger may not be initialized yet when used,
+  /// so it's better to await [apiLoggerAsync] to complete.
+  DebugLogger get apiLogger => _loggers.containsKey(_apiLoggerName)
+      ? _loggers[_apiLoggerName]!
+      : throw StateError('API Logger not initialized yet, either await for `apiLoggerAsync` or await for `init()` to complete');
 
-  /// List of names of loggers to be created in [named] constructor
-  final List<String> names;
+  /// Async logger created in the default constructor used for logging generic API events.
+  /// This logger is initialized immediately in the constructor, you need to await for it to complete
+  /// when needed. If you need logger without waiting, use [apiLogger] instead. It's your responsibility
+  /// to make sure the logger is initialized, eg. by awaiting for `init()` to complete.
+  Future<DebugLogger> get apiLoggerAsync => _loggersCompleters.containsKey(_apiLoggerName)
+      ? _loggersCompleters[_apiLoggerName]!.future
+      : throw StateError('API Logger does not exist');
+
+  /// Logger created in the default constructor used for logging performance.
+  /// If loggers are initialized from constructor, this logger may not be initialized yet when used,
+  /// so it's better to await [performanceLoggerAsync] to complete.
+  DebugLogger get performanceLogger => _loggers.containsKey(_performanceLoggerName)
+      ? _loggers[_performanceLoggerName]!
+      : throw StateError(
+          'Performance Logger not initialized yet, either await for `performanceLoggerAsync` or await for `init()` to complete');
+
+  /// Async logger created in the default constructor used for logging performance.
+  /// This logger is initialized immediately in the constructor, you need to await for it to complete
+  /// when needed. If you need logger without waiting, use [performanceLogger] instead. It's your responsibility
+  /// to make sure the logger is initialized, eg. by awaiting for `init()` to complete.
+  Future<DebugLogger> get performanceLoggerAsync => _loggersCompleters.containsKey(_performanceLoggerName)
+      ? _loggersCompleters[_performanceLoggerName]!.future
+      : throw StateError('Performance Logger does not exist');
+
+  /// Logger created in the default constructor used for logging errors.
+  /// If loggers are initialized from constructor, this logger may not be initialized yet when used,
+  /// so it's better to await [errorLoggerAsync] to complete.
+  DebugLogger get errorLogger => _loggers.containsKey(_errorLoggerName)
+      ? _loggers[_errorLoggerName]!
+      : throw StateError('Error Logger not initialized yet, either await for `errorLoggerAsync` or await for `init()` to complete');
+
+  /// Async logger created in the default constructor used for logging errors.
+  /// This logger is initialized immediately in the constructor, you need to await for it to complete
+  /// when needed. If you need logger without waiting, use [performanceLogger] instead. It's your responsibility
+  /// to make sure the logger is initialized, eg. by awaiting for `init()` to complete.
+  Future<DebugLogger> get errorLoggerAsync => _loggersCompleters.containsKey(_errorLoggerName)
+      ? _loggersCompleters[_errorLoggerName]!.future
+      : throw StateError('Error Logger does not exist');
+
+  /// Returns a logger created in the named constructor with the given name. If there is no such logger throws an exception.
+  /// If loggers are initialized from constructor, this logger may not be initialized yet when used,
+  /// so it's better to await [loggerAsync(name)] to complete.
+  DebugLogger logger(String name) =>
+      _loggers.containsKey(name) ? _loggers[name]! : throw Exception('Logger $name does not exist or is not initialized yet');
+
+  Map<String, DebugLogger> get loggers => _loggers.length == _names.length
+      ? _loggers
+      : throw StateError('Loggers are not initialized yet, either await for `loggersAsync` or await for `init()` to complete');
+  final _loggers = <String, DebugLogger>{};
+
+  /// Async logger created in the named constructor with the given name. If there is no such logger throws an exception.
+  /// This logger is initialized immediately in the constructor, you need to await for it to complete
+  /// when needed. If you need logger without waiting, call [logger(name)] instead. It's your responsibility
+  /// to make sure the logger is initialized, eg. by awaiting for `init()` to complete.
+  FutureOr<DebugLogger> loggerAsync(String name) =>
+      _loggersCompleters.containsKey(name) ? _loggersCompleters[name]!.future : throw Exception('Logger $name does not exist');
+
+  Map<String, Future<DebugLogger>> get loggersAsync => _loggersCompleters.map((k, v) => MapEntry(k, v.future));
+  final _loggersCompleters = <String, Completer<DebugLogger>>{};
+
+  /// List of names of loggers created in [named] constructor
+  List<String> get names => _names;
+  final List<String> _names;
 
   /// Name of root media directory used for storing logs. Should be one of
   /// [MediaStorage] directories, default is [MediaStorage.DIRECTORY_DOWNLOADS]
-  final String root;
+  String get root => _root;
+  final String _root;
 
-  /// Path under the root directory used for storing logs. Should contain {pkg}
+  /// Path under the root directory used for storing logs. May contain {pkg}
   /// place holder which is replaces with the package name. Default is '{pkg}/files/logs'
-  final String path;
+  String get path => _path;
+  final String _path;
 
   /// Minimum log level
-  Level? level;
+  Level? get level => _level;
+  final Level? _level;
 
-  /// Map of created loggers, the keys are loggers names passed in the [named]
-  /// constructor
-  final loggers = <String, DebugLogger>{};
+  bool _initialized = false;
 
-  /// Constructor used for creating named loggers
+  /// Constructor used for creating named loggers. You need to specify at least list of names. Optionally
+  /// you can specify [root] directory and [path]. If not specified, default values will be used.
+  /// If you want to initialize loggers immediately, set [initialize] to true. The loggers will be initialized
+  /// asynchronously so you need to await for them to complete in your code then.
+  /// It's better to leave [initialize] as false and call and await [init()] in your code.
+  ///
+  /// Example
+  /// -------
+  /// ```dart
+  /// DebugService debugService = DebugService.named(
+  ///   names: [ 'app', 'debug' ],
+  ///   path: 'files/logs', initialize: true,
+  /// );
+  ///
+  /// // next call might throw an exception, logger might not be initialized yet
+  /// debug.service.logger('app').log('Hello');
+  ///
+  /// // await for logger to be initialized
+  /// final logger = await debugService.loggerAsync('app');
+  /// logger.log('Hello'); // use logger
+  ///
+  /// // await and use logger
+  /// debugService.loggerAsync('debug').then((logger) => logger.log('Hello'));
+  /// ````
   DebugService.named({
+    required List<String> names,
     String? root,
-    this.level,
-    this.path = defaultPath,
-    required this.names,
-  }) : root = root ?? MediaStorage.DIRECTORY_DOWNLOADS {
-    _init();
+    String? path,
+    Level? level,
+    bool initialize = false,
+  })  : _root = root ?? MediaStorage.DIRECTORY_DOWNLOADS,
+        _level = level,
+        _path = path ?? defaultPath,
+        _names = names {
+    for (var name in _names) {
+      _loggersCompleters.putIfAbsent(name, () => Completer<DebugLogger>());
+    }
+    if (initialize) {
+      init();
+    }
   }
 
-  /// Constructor creating default loggers
+  /// Constructor creating default loggers ([appLogger], [apiLogger], [performanceLogger] and [errorLogger]).
+  /// See [DebugService.named] for more details
   DebugService({
     String? root,
-    this.level,
-    this.path = defaultPath,
-  })  : root = root ?? MediaStorage.DIRECTORY_DOWNLOADS,
-        names = ['app', 'api', 'performance', 'error'] {
-    _init().then((_) {
-      appLogger = loggers['app']!;
-      apiLogger = loggers['api']!;
-      performanceLogger = loggers['performance']!;
-      errorLogger = loggers['error']!;
-    });
-  }
+    String? path,
+    Level? level,
+    bool initialize = false,
+  }) : this.named(
+          names: [_appLoggerName, _apiLoggerName, _performanceLoggerName, _errorLoggerName],
+          root: root,
+          path: path,
+          level: level,
+          initialize: initialize,
+        );
 
-  Future<bool> _init() async {
+  /// Initialize loggers
+  ///
+  Future<bool> init() async {
+    if (_initialized) {
+      // already initialized
+      return true;
+    }
     String dir;
-    if (path.contains('{pkg}')) {
+    if (_path.contains('{pkg}')) {
       var pkg = await PackageInfo.fromPlatform();
-      dir = path.replaceAll('{pkg}', pkg.packageName);
+      dir = _path.replaceAll('{pkg}', pkg.packageName);
     } else {
-      dir = path;
+      dir = _path;
     }
 
-    final rootDir = await MediaStorage.getExternalStoragePublicDirectory(root);
+    final rootDir = await MediaStorage.getExternalStoragePublicDirectory(_root);
     if (await MediaStorage.getRequestStoragePermission()) {
       try {
         await MediaStorage.CreateDir(rootDir, dir);
       } catch (e) {
         debugPrint('$e');
+        _initialized = true;
         return false;
       }
-      for(final name in names) {
+      for (final name in _names) {
         final file = File('$rootDir/$dir/$name.log');
-        loggers[name] = DebugLogger(
+        _loggers[name] = DebugLogger(
           name: name,
-          level: level,
+          level: _level,
           output: MultiFileOutput(file: file),
         );
       }
+      _initialized = true;
       return true;
     } else {
+      _initialized = true;
       return false;
     }
   }
