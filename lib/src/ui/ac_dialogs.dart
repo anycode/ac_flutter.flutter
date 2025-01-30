d7a9eac3019dea4b7536c3ede4c0fb10c4b8c375
Martin Edlman <ac@anycode.dev>
2025-01-30 15:22:26 +0100

    Utility functions for easier creating alert and confirm dialogs
    Signed-off-by: Martin Edlman <ac@anycode.dev>

---
+++ lib/src/ui/ac_dialogs.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+Future showAcAlertDialog({
+  required BuildContext context,
+  required String title,
+  required String message,
+  required String buttonText,
+  Widget? content,
+  bool barrierDismissible = true,
+  Color? barrierColor,
+  String? barrierLabel,
+  bool useSafeArea = true,
+  bool useRootNavigator = true,
+  RouteSettings? routeSettings,
+  Offset? anchorPoint,
+  TraversalEdgeBehavior? traversalEdgeBehavior,
+}) {
+  return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: Text(title),
+            content: content ?? Text(message),
+            actions: [TextButton(onPressed: () => Navigator.pop(context, true), child: Text(buttonText))],
+          ),
+      barrierDismissible: barrierDismissible,
+      barrierColor: barrierColor,
+      barrierLabel: barrierLabel,
+      useSafeArea: useSafeArea,
+      useRootNavigator: useRootNavigator,
+      routeSettings: routeSettings,
+      anchorPoint: anchorPoint,
+      traversalEdgeBehavior: traversalEdgeBehavior);
+}
+
+Future<bool?> showAcConfirmDialog({
+  required BuildContext context,
+  required String title,
+  String message = '',
+  required String confirmText,
+  required String cancelText,
+  Widget? content,
+  bool barrierDismissible = true,
+  Color? barrierColor,
+  String? barrierLabel,
+  bool useSafeArea = true,
+  bool useRootNavigator = true,
+  RouteSettings? routeSettings,
+  Offset? anchorPoint,
+  TraversalEdgeBehavior? traversalEdgeBehavior,
+}) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: Text(title),
+            content: content ?? Text(message),
+            actions: [
+              TextButton(onPressed: () => Navigator.pop(context, false), child: Text(cancelText)),
+              TextButton(onPressed: () => Navigator.pop(context, true), child: Text(confirmText)),
+            ],
+          ),
+      barrierDismissible: barrierDismissible,
+      barrierColor: barrierColor,
+      barrierLabel: barrierLabel,
+      useSafeArea: useSafeArea,
+      useRootNavigator: useRootNavigator,
+      routeSettings: routeSettings,
+      anchorPoint: anchorPoint,
+      traversalEdgeBehavior: traversalEdgeBehavior);
+}
